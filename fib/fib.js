cc5a4b70cce63c39bd647f140009cf3da7288aa2
John A. Squier <jasquier@gmail.com>
2024-06-22 15:28:09 -0400

    Change fib to memoized impl

--- fib/fib.js
+++ fib/fib.js
@@ -1,8 +1,14 @@
+const cache = {};
+
 function fib(n) {
+  if (n in cache) return cache[n];
   if (n <= 2) return 1;
-  return fib(n - 1) + fib(n - 2);
+
+  cache[n] = fib(n - 1) + fib(n - 2);
+  return cache[n];
 }
 
 console.log(fib(6));
 console.log(fib(7));
 console.log(fib(8));
+console.log(fib(50));
