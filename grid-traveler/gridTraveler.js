eb652fa0a398e1686cf34181162ebce094ed5699
John A. Squier <jasquier@gmail.com>
2024-07-06 16:26:13 -0400

    Change gridTraveler to memoized impl

--- grid-traveler/gridTraveler.js
+++ grid-traveler/gridTraveler.js
@@ -1,7 +1,18 @@
+const cache = {};
+
+function makeKey(m, n) {
+  return m <= n ? `${m},${n}` : `${n},${m}`;
+}
+
 function gridTraveler(m, n) {
+  const key = makeKey(m, n);
+  if (key in cache) return cache[key];
+
   if (m === 0 || n === 0) return 0;
   if (m === 1 && n === 1) return 1;
-  return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+
+  cache[key] = gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+  return cache[key];
 }
 
 console.log(gridTraveler(1, 1));
@@ -9,4 +20,4 @@ console.log(gridTraveler(2, 1));
 console.log(gridTraveler(1, 2));
 console.log(gridTraveler(2, 2));
 console.log(gridTraveler(2, 3));
-// console.log(gridTraveler(18, 18).toLocaleString());
+console.log(gridTraveler(18, 18).toLocaleString());
