d07d6e82dfd67c5b31faaf8dd21c2063d0139626
John A. Squier <jasquier@gmail.com>
2024-09-12 10:05:59 -0400

    Add naive canSum impl

---
+++ canSum/README.md
@@ -0,0 +1,11 @@
+# Can Sum?
+
+Write a function `canSum(targetSum, number)` that takes in a target sum and an
+array of numbers as arguments.
+
+The function should return a boolean indicating whether or not it is possible to
+generate the target sum using the numbers from the array.
+
+You may use an element of the array as many times as needed.
+
+You may assume that all input numbers are non-negative.
---
+++ canSum/canSum.js
@@ -0,0 +1,22 @@
+function canSum(targetSum, parts, cache = {}) {
+  // if (targetSum in cache) return cache[targetSum];
+  if (targetSum === 0) return true;
+
+  const choices = parts.filter((part) => part <= targetSum);
+
+  for (let choice of choices) {
+    if (canSum(targetSum - choice, parts, cache)) {
+      // cache[targetSum] = true;
+      return true;
+    }
+  }
+
+  // cache[targetSum] = false;
+  return false;
+}
+
+console.log(canSum(7, [2, 3])); // true
+console.log(canSum(7, [5, 3, 4, 7])); // true
+console.log(canSum(7, [2, 4])); // false
+console.log(canSum(8, [2, 3, 5])); // true
+console.log(canSum(300, [7, 14])); // false
