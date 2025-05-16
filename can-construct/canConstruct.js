62ca7723cd72146a411f09173c7ec8ce234e0568
John A. Squier <jasquier@gmail.com>
2025-05-16 09:20:01 -0400

    Change canConstruct to memoized impl

--- can-construct/canConstruct.js
+++ can-construct/canConstruct.js
@@ -1,14 +1,32 @@
-function canConstruct(target, wordBank) {
+function canConstruct(target, words, cache = {}) {
+  if (target in cache) return cache[target];
   if (target === "") return true;
 
-  const choices = wordBank.filter((word) => target.startsWith(word));
+  const choices = words.filter((word) => target.startsWith(word));
 
   for (const choice of choices) {
     const suffix = target.slice(choice.length);
-    if (canConstruct(suffix, wordBank)) return true;
+    if (canConstruct(suffix, words, cache)) return true;
   }
 
+  cache[target] = false;
   return false;
 }
 
 console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(
+  canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]),
+);
+console.log(
+  canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]),
+);
+console.log(
+  canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeee",
+  ]),
+);
