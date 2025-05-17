398349b0fac0b896eb2e82d49e5eb7b4e86dcc8d
John A. Squier <jasquier@gmail.com>
2025-05-17 15:03:12 -0400

    Add tabulation strategy for canSum

--- src/can-sum/canSum.ts
+++ src/can-sum/canSum.ts
@@ -19,8 +19,24 @@ function canSum(
   return false;
 }
 
+function canSumIter(targetSum: number, parts: number[]): boolean {
+  const table = Array(targetSum + 1).fill(false);
+  table[0] = true;
+
+  for (let i = 0; i <= targetSum; i++) {
+    if (table[i] === true) {
+      for (let part of parts) {
+        table[i + part] = true;
+      }
+    }
+  }
+
+  return table[targetSum];
+}
+
 console.log(canSum(7, [2, 3])); // true
 console.log(canSum(7, [5, 3, 4, 7])); // true
 console.log(canSum(7, [2, 4])); // false
 console.log(canSum(8, [2, 3, 5])); // true
 console.log(canSum(300, [7, 14])); // false
+console.log(canSumIter(300, [7, 14])); // false
