e2b7a2818ba86d3c3e2133bf9e9332834bf138f7
John A. Squier <jasquier@gmail.com>
2025-05-17 10:32:35 -0400

    Add tabulation strategy for fib

--- src/fib/fib.ts
+++ src/fib/fib.ts
@@ -7,7 +7,20 @@ function fib(n: number, cache: Record<number, number> = {}): number {
   return result;
 }
 
+function fibIter(n: number) {
+  const table = Array(n + 1).fill(0);
+  table[1] = 1;
+
+  for (let i = 0; i <= n; i++) {
+    table[i + 1] += table[i];
+    table[i + 2] += table[i];
+  }
+
+  return table[n];
+}
+
 console.log(fib(6));
 console.log(fib(7));
 console.log(fib(8));
 console.log(fib(50));
+console.log(fibIter(100));
