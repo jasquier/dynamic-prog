45ce4a6bdb5d7aac0c4f9b2454b0fd0a0af4b052
John A. Squier <jasquier@gmail.com>
2025-05-20 07:04:16 -0400

    Add tabulation strategy for howSum

--- src/how-sum/howSum.ts
+++ src/how-sum/howSum.ts
@@ -20,8 +20,25 @@ function howSum(
   return null;
 }
 
+function howSumIter(targetSum: number, numbers: number[]): number[] | null {
+  const table: (number[] | null)[] = Array(targetSum + 1).fill(null);
+  table[0] = [];
+
+  for (let i = 0; i <= targetSum; i++) {
+    const ele = table[i];
+    if (Array.isArray(ele)) {
+      for (let num of numbers) {
+        table[i + num] = ele.concat(num);
+      }
+    }
+  }
+
+  return table[targetSum] ?? null;
+}
+
 console.log(howSum(7, [2, 3]));
 console.log(howSum(7, [5, 3, 4, 7]));
 console.log(howSum(7, [2, 4]));
 console.log(howSum(8, [2, 3, 5]));
 console.log(howSum(300, [7, 14]));
+console.log(howSumIter(300, [7, 14]));
