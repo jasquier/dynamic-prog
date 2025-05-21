be9c17b19dc5258992feed4caeb1f99531ec43ea
John A. Squier <jasquier@gmail.com>
2025-05-21 12:10:33 -0400

    Add tabulation strategy for allConstruct

--- src/all-construct/allConstruct.test.ts
+++ src/all-construct/allConstruct.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert/strict";
-import { allConstruct } from "./allConstruct.ts";
+import { allConstruct, allConstructIter } from "./allConstruct.ts";
 
 type AllConstructInput = [target: string, words: string[]];
 type AllConstructTestPair = [input: AllConstructInput, expected: string[][]];
@@ -19,18 +19,33 @@ describe("allConstruct", () => {
           ["enter", "a", "p", "o", "t", "ent", "p", "o", "t"],
         ],
       ],
-      [
-        [
-          "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
-          ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"],
-        ],
-        [],
-      ],
+      [["eeeeeeeeeeef", ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"]], []],
     ];
 
     pairs.forEach(([input, expected]) => {
       const [target, words] = input;
       assert.deepEqual(allConstruct(target, words), expected);
+      // The iterative version does not return the solutions in the same order as the recursive version.
+      const iterSolutions = allConstructIter(target, words);
+      expected.forEach((solution) => {
+        findMatchForIn(solution, iterSolutions);
+      });
     });
   });
 });
+
+// Find a match for solution in solutions.
+function findMatchForIn(solution: string[], solutions: string[][]) {
+  if (!solutions.some((s) => arrayContentsEqual(s, solution))) {
+    throw Error(`Could not find match for ${solution} in ${solutions}`);
+  }
+}
+
+function arrayContentsEqual(a1: any[], a2: any[]) {
+  try {
+    assert.deepEqual(a1, a2);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
--- src/all-construct/allConstruct.ts
+++ src/all-construct/allConstruct.ts
@@ -22,3 +22,22 @@ export function allConstruct(
   cache[target] = result;
   return result;
 }
+
+export function allConstructIter(target: string, words: string[]) {
+  const table = Array(target.length + 1)
+    .fill(null)
+    .map((_) => Array<string[]>());
+  table[0] = [[]];
+
+  for (let i = 0; i <= target.length; i++) {
+    const ele = table[i] ?? [];
+    for (const word of words) {
+      if (target.slice(i, i + word.length) === word) {
+        const newCombos = ele.map((subArray) => [...subArray, word]);
+        table[i + word.length]?.push(...newCombos);
+      }
+    }
+  }
+
+  return table[target.length] ?? [];
+}
